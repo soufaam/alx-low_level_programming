15a67b252fc0ae52cfcd2b80fde099cc0dd01c52
soufiane aamari <soufianeaamari@gmail.com>
2023-03-18 08:49:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{       char mychar[] = "\"Programming is like building a multilingual puzzle";
+	puts(mychar);
+    return (0);
+}
+
