5678b71dd0c10f3136520890296e50c5f63aa4b0
soufiane aamari <soufianeaamari@gmail.com>
2023-03-20 15:58:24 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: 'the program's description
+*  Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = '0';
+
+	for (; ch <= '9'; ch++)
+		putchar(ch);
+
+	putchar ('\n');
+	return (0);
+}
