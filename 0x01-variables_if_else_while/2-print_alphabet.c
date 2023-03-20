459c781a299c91ca8bfeb9df516a3dc48bfdd5e1
soufiane aamari <soufianeaamari@gmail.com>
2023-03-20 07:37:46 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,23 +10,12 @@
 
 int main(void)
 {
-	int n;
-	int r;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	r = n % 10;
-	if (r > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, r);
-	}
-	else if (r < 6 && r != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, r);
-	}
-	else if (r == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, r);
-	}
+	for (i; i < 27; i++)
+		putchar(alpha[i]);
+
+	putchar ('\n');
 	return (0);
 }
