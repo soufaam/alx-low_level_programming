b3fe61e50465b54ac10623e6c6fc127e0b01b34c
soufiane aamari <soufianeaamari@gmail.com>
2023-03-20 16:06:25 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: 'the program's description
+*  Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	for (; num <= 9; num++)
+		putchar('0' + num);
+
+	putchar ('\n');
+	return (0);
+}
