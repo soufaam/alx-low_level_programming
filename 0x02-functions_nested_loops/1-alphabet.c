6ad2d550517201b5f58cb927d2233c1f3cf99c3c
soufiane aamari <soufianeaamari@gmail.com>
2023-03-21 16:58:04 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,7 +12,7 @@ void print_alphabet(void)
 {
 	char ch = 'a';
 
-        for (; ch <= 'z' ; ch++ )
-                _putchar(ch);
-        _putchar('\n');
+	for (; ch <= 'z' ; ch++)
+		_putchar(ch);
+	_putchar('\n');
 }
