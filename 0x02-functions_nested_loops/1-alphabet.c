48df63869b9ebb1d4504a5ae7187ac396e7bb43c
soufiane aamari <soufianeaamari@gmail.com>
2023-03-21 16:47:53 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+* Description: 'the program's description
+*  Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (; ch <= 'z' ; ch++ )
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
