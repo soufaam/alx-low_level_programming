1405557384650cb466e9ba2dabab37ef70742735
soufiane aamari <soufianeaamari@gmail.com>
2023-03-21 16:33:29 -0700

    adding prototypes and main header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,6 @@
-#include <unistd.h>
+#ifndef MAIN_H_
+#define MAIN_H_
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+
+#endif
