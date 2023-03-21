4a6292ae8d0ccf853359dafc7260bf586e03d16b
soufiane aamari <soufianeaamari@gmail.com>
2023-03-21 03:55:52 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: 'the program's description
+*  Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	for (; num <= 9; num++)
+	{
+		putchar('0' + num);
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
