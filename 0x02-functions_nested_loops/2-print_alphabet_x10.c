adcdfd3b1fb0833e5f3bf32a3ea6811983ec2187
soufiane aamari <soufianeaamari@gmail.com>
2023-03-22 04:14:51 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,13 @@
 
 void print_alphabet_x10(void)
 {
-	char ch = 'a';
+	char  ch;
 	int i = 0;
 
 	for (; i < 10; i++)
-		for (; ch <= 'z' ; ch++)
-			_putchar(ch);
-	_putchar('\n');
+	{
+			for (ch = 'a'; ch <= 'z' ; ch++)
+				_putchar(ch);
+			_putchar('\n');
+	}
 }
