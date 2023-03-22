c437cd413915f4789dcb64faa14bd6e3c9ae9162
soufiane aamari <soufianeaamari@gmail.com>
2023-03-21 17:11:10 -0700

    2-main.c 2-printx10

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+* Description: 'the program's description
+*  Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_alphabet_x10  - check the code
+* Description: 'this program print alphabet from a to z 10 times'
+*  Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int i = 0;
+
+	for (; i < 10; i++)
+		for (; ch <= 'z' ; ch++)
+			_putchar(ch);
+	_putchar('\n');
+}
