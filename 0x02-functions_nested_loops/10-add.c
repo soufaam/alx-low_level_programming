05620e08887674b25d1f07c2a62be9862d3cfd38
soufiane aamari <soufianeaamari@gmail.com>
2023-03-23 05:38:29 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add  - check the code
+* @n: n is int variable
+* @m: m is int variable
+* Description: 'this program add numbers'
+*  Return: Always 0 (Success)
+*/
+
+int add(int n, int m)
+{
+	return (n + m);
+}
