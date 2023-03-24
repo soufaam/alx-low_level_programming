68fd93a5f43d38bf93aa961843666c818141221f
soufiane aamari <soufianeaamari@gmail.com>
2023-03-24 05:02:08 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,10 +15,7 @@ void print_natural(void)
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
-		{
 			sum = sum + i;
-			printf("%d, ", i);
-		}
 	}
 	printf("%d", sum);
 	printf("\n");
