fa0f635460f701a72771d22af4404a5afaeb1e31
soufiane aamari <soufianeaamari@gmail.com>
2023-03-25 03:27:55 -0700

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int fi, f0 = 1, f1 = 2, sum = 0;
+
+	fi = f0 + f1;
+	while (fi <= 4000000)
+	{
+		f0 = f1;
+		f1 = fi;
+		if (fi % 2 == 0)
+			sum = sum + fi;
+		fi = f0 + f1;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
