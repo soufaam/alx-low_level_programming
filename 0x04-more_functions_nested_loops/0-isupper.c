5631c41cf4142d5577fa41ccfb4f8e6b89a42ff1
soufiane aamari <soufianeaamari@gmail.com>
2023-03-26 04:00:36 -0700

    0-isupper betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,9 +3,9 @@
 #include "main.h"
 
 /**
-*  _islower  - check the code
+*  _isupper  - check the code
 * @c: c is char variable
-* Description: 'this program _islower case orupper case from a to z 10 times'
+* Description: 'this program _isupper case orupper case from a to z 10 times'
 *  Return: Always 0 (Success)
 */
 
