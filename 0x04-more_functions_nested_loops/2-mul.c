c52484ce70d02f75c21a1270c384a2bd59b75a98
soufiane aamari <soufianeaamari@gmail.com>
2023-03-26 04:13:31 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*  mul  - check the code
+* @a: a is int variable 
+* @b: b is int variable
+* Description: 'this program mul case orupper case from a to z 10 times'
+*  Return: Always 0 (Success)
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
