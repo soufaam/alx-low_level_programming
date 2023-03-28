556ccf99376a05d00f7a2b9575fe65b147700f9c
soufiane aamari <soufianeaamari@gmail.com>
2023-03-28 05:41:50 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* reset_to_98 -  Entrypoint
+* Description: 'the program's description reset_to_98
+* @n : 1 param
+*  Return: Always 0 (Success)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#include <math.h>
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
