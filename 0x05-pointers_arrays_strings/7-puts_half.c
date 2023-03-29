f32f9b1e2b7d276a70a21f46b2003b72bb5b4723
soufiane aamari <soufianeaamari@gmail.com>
2023-03-29 04:57:29 -0700

    second half str

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* _strlen -  Entrypoint
+* Description: 'the program's description _strlen
+* @s : 1 param
+*  Return: Always 0 (Success)
+*/
+
+int _strlen(char *s)
+{
+	int t = 0, len = 0;
+
+	while (*(s + t) != '\0')
+	{
+		len = len + 1;
+		t++;
+	}
+	return (len);
+}
+
+/**
+* puts_half -  Entrypoint
+* Description: 'the program's description puts_half
+* @str : 1 param
+*  Return: Always 0 (Success)
+*/
+
+void puts_half(char *str)
+{
+	int n, len;
+
+	len = _strlen(str);
+	n = len / 2;
+	while (*(str + n) != '\0')
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
