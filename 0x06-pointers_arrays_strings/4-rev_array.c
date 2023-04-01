839f34ebfccb1ef759d94ab1a60e30a885db30b0
soufiane aamari <soufianeaamari@gmail.com>
2023-04-01 05:20:53 -0700

    reverse_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* reverse_array -  Entrypoint
+* Description: 'the program's description reverse_array
+* @a : 1 param
+* @n : int value
+*  Return: Always 0 (Success)
+*/
+
+void reverse_array(int *a, int n)
+{
+	int j, tmp;
+
+	for (j = 0; j <= n / 2; j++)
+	{
+		tmp = *(a + n -1 - j);
+		*(a + n -1 - j) = *(a + j);
+		*(a + j) = tmp;
+	}
+}
