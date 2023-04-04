731978a8614831cc3dd3629ede1034772acc6999
soufiane aamari <soufianeaamari@gmail.com>
2023-04-04 03:42:09 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is README file for 0x07-pointers_arrays_strings 
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#include <math.h>
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
