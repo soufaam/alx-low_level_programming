3a945b4dfeb0ba2ccccd41d34fa401e146dfaade
soufiane aamari <soufianeaamari@gmail.com>
2023-04-04 16:45:08 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* _strstr -  Entrypoint
+* Description: 'the program's description _strstr
+* @haystack : 1 param
+* @needle : 2 param
+*  Return: Always 0 (Success)
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = NULL;
+	int i = 0, j = 0;
+
+	while (*(needle + i) != '\0')
+	{
+		while ((*(haystack + j)) != '\0')
+		{
+			if (*(haystack + j) == *(needle + i))
+			{
+				if (result == NULL)
+				{
+					result = haystack + j;
+					j++;
+					break;
+				}
+				else
+				{
+					j++;
+					break;
+				}
+			}
+			else
+			{
+				j++;
+				result = NULL;
+				break;
+			}
+		}
+		if (result == NULL)
+		{
+			i = 0;
+			if (*(haystack + j) == '\0')
+				break;
+			continue;
+		}
+		i++;
+	}
+	return (result);
+}
