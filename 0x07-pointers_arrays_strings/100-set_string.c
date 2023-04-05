4c72a137607bf44e9a1fd8220437d5732735ecb0
soufiane aamari <soufianeaamari@gmail.com>
2023-04-05 08:54:22 -0700

    double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* set_string -  Entrypoint
+* Description: 'the program's description set_string
+* @s : 1 param
+* @to : 2 param
+*  Return: Always 0 (Success)
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
