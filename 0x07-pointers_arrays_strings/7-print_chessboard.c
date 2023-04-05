11bbd38fe7bded328c98f4628464ee359209e3bc
soufiane aamari <soufianeaamari@gmail.com>
2023-04-04 17:19:36 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* print_chessboard -  Entrypoint
+* Description: 'the program's description print_chessboard
+* @a : 1 param
+* Return: Always 0 (Success)
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+	
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(*(*(a + i) + j));
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	return ;
+}
