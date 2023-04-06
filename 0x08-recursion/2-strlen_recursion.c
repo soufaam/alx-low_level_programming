c6470788dca2551ddf7fe5633d9e8ab37f5c7b57
soufiane aamari <soufianeaamari@gmail.com>
2023-04-06 06:32:04 -0700

    devlopping 2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* _strlen_recursion -  Entrypoint
+* Description: 'the program's description _strlen__recursion
+* @s: 1 param
+* Return: Always 0 (Success)
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return(1 + _strlen_recursion(s + 1));
+	}
+	return(0);
+}
