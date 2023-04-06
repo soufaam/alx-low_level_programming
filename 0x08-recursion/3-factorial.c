8c4078091d7ea48eeef19e2c2942cbfae050c0bc
soufiane aamari <soufianeaamari@gmail.com>
2023-04-06 06:44:33 -0700

    3-factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* factorial -  Entrypoint
+* Description: 'the program's description factorial
+* @n: 1 param
+* Return: Always 0 (Success)
+*/
+
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	return (1);
+}
