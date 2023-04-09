950b951e173df6215f95195f2b45d82663db52ee
soufiane aamari <soufianeaamari@gmail.com>
2023-04-09 08:07:17 -0700

    first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
