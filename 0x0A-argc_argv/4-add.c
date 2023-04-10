4077d1ae335cc04fc5acd0f97a60a2d6be18fc43
soufiane aamari <soufianeaamari@gmail.com>
2023-04-09 17:14:02 -0700

    fourth task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *@argc: first param
+ *@argv: 2 param
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	int sum = 0, i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i <= argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("error\n");
+			return (0);
+		}
+		sum = sum + atoi(argv[i]);
+	}
+		printf("%d\n", sum);
+	return (0);
+}
