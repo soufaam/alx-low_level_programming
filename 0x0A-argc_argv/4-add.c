2aaf8c432ae14e8a0e2834f7b9cf6773f31cb9d3
soufiane aamari <soufianeaamari@gmail.com>
2023-04-09 17:18:30 -0700

    fourth task fixing error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,12 +11,12 @@ int main(int argc, char __attribute__ ((unused)) *argv[])
 {
 	int sum = 0, i;
 
-	if (argc == 1)
+	if (argc <= 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for (i = 1; i <= argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		if (atoi(argv[i]) == 0)
 		{
