841ae54b9e464d80ea18a16ff8850a676554a1c9
soufiane aamari <soufianeaamari@gmail.com>
2023-04-14 04:56:05 -0700

    create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* create_array -  Entrypoint
+* Description: 'the program's description create_array
+* @size : 1 param
+* @c: 2 param
+*  Return: Always 0 (Success)
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size + 1);
+	for (; i < size; i++)
+	{
+		*(array + i) = c;
+	}
+	return (array);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#include <math.h>
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif /* MAIN_H */
