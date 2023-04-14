48bc0fcdfc0128bad08f970f1adb44908f3fc162
soufiane aamari <soufianeaamari@gmail.com>
2023-04-14 07:51:28 -0700

    developping strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* _strlen -  Entrypoint
+* Description: 'the program's description _strlen
+* @s : 1 param
+*  Return: Always 0 (Success)
+*/
+
+int _strlen(char *s)
+{
+	int t = 0, len = 0;
+
+	while (*(s + t) != '\0')
+	{
+		len = len + 1;
+		t++;
+	}
+	return (len);
+}
+
+/**
+* _strdup -  Entrypoint
+* Description: 'the program's description _strdup
+* @str : 1 param
+*  Return: Always 0 (Success)
+*/
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0;
+
+	s = malloc(sizeof(char) * (_strlen(str)));
+	if (s)
+	{
+		for (; i < _strlen(str); i++)
+		{
+			*(s + i) = *(str + i);
+		}
+	return (s);
+	}
+	free(s);
+	return (NULL);
+}
