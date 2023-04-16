e3043e0718b4a5391d4edf6d9a506933da30771b
soufiane aamari <soufianeaamari@gmail.com>
2023-04-16 07:13:19 -0700

    concatenatinf string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* _strlen -  Entrypoint
+* Description: 'the program's description _strlen
+* @s : 1 param
+*  Return: Always 0 (Success)
+*/
+
+int _strlen(char *s)
+{
+	int t = 0, len = 0;
+
+	while (*(s + t) != '\0')
+	{
+		len = len + 1;
+		t++;
+	}
+	return (len);
+}
+
+/**
+* str_concat -  Entrypoint
+* Description: 'the program's description str_concat
+* @s1 : 1 param
+* @s2 : 1 param
+*  Return: Always 0 (Success)
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i = 0, j = 0;
+
+	s = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2)) + 1);
+	if (s)
+	{
+		while (*(s1 + i) != '\0')
+		{
+			*(s + i) = *(s1 + i);
+			i++;
+		}
+		while (*(s2 + j) != '\0')
+		{
+			*(s + i) = *(s2 + j);
+			i++;
+			j++;
+		}
+		*(s + i) = '\0';
+		return (s);
+	}
+	free(s);
+	return (NULL);
+	}
