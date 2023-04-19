ed756a42a0fb7e8f7b32869e74f0972d7bfb41fd
soufiane aamari <soufianeaamari@gmail.com>
2023-04-19 04:39:33 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H_
+#define DOG_H_
+#include <stdio.h>
+
+int _putchar(char c);
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* DOG_H_ */
