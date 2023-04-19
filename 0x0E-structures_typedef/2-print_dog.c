da5c44d58a93a21d99774f0614b6ecaad50b28f6
soufiane aamari <soufianeaamari@gmail.com>
2023-04-19 05:50:44 -0700

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include "dog.h"
+
+/**
+* print_dog -  Entrypoint
+* Description: 'the program's description print_dog
+* @d : 1 param
+*  Return: Always 0 (Success)
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+	if (d->name == NULL)
+	{
+		d->name = "(nill)";
+	}
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
