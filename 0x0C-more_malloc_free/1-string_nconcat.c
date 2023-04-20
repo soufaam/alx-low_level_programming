937a24ce9922c8914882c5faac87cb08d41247af
soufiane aamari <soufianeaamari@gmail.com>
2023-04-20 15:42:00 -0700

    nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* _strlen -  Entrypoint
+* Description: 'the program's description _strlen
+* @s : 1 param
+*  Return: Always 0 (Success)
+*/
+
+int _strlen(char *s)
+{
+	int t = 0, len = 0;
+
+	if (s == NULL)
+		return (len);
+	while (*(s + t) != '\0')
+	{
+		len = len + 1;
+		t++;
+	}
+	return (len);
+}
+
+/**
+* string_nconcat -  Entrypoint
+* Description: 'the program's description string_nconcat
+* @s1 : 1 param
+* @s2 : 2 param
+* @n : 3 param 
+*  Return: Always 0 (Success)
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int i = 0, j = 0, m = 0, len = 0;
+	
+	len = (unsigned int)_strlen(s2);
+	if (n >  len)
+		m = len;
+	else 
+		m = n;
+	s = malloc(sizeof(char) * (_strlen(s1) + m) + 1);
+	if (s)
+	{
+		if (s1 != NULL)
+		{
+			while (*(s1 + i) != '\0')
+			{
+				*(s + i) = *(s1 + i);
+				i++;
+			}
+		}
+		if (s2 != NULL)
+		{
+			while ( j < m)
+			{
+				*(s + i) = *(s2 + j);
+				i++;
+				j++;
+			}
+		}
+		*(s + i) = '\0';
+		return (s);
+	}
+	return (NULL);
+	}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,4 +4,5 @@
 #include <math.h>
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif /* MAIN_H */
