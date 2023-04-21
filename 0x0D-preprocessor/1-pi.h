a8c69112a1d7fe5e292eb44a050e1d52120987d9
soufiane aamari <soufianeaamari@gmail.com>
2023-04-21 04:51:20 -0700

    define PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,7 @@
+#ifndef 1-PI_H_
+#define 1-PI_H_
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265359
+#endif /* MAIN_H */
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+this is 0x05. C more  0x0C. C - malloc, free  README.md
