89be678634e9160d6bd735fb2fbe6fc72f84f3a6
soufiane aamari <soufianeaamari@gmail.com>
2023-04-21 03:25:41 -0700

    array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* array_range -  Entrypoint
+* Description: 'the program's description array_range
+* @min : 1 param
+* @max: 2 param
+*  Return: Always 0 (Success)
+*/
+int *array_range(int min, int max)
+{
+	int i, val;
+	int *array = NULL;
+
+	val = max - min + 1;
+	if (val <= 0)
+		return (NULL);
+	array = malloc (val * sizeof(int));
+	if (!array)
+		return (NULL);
+	for (i = 0; i <= val; i++)
+		array[i] = min + i ;
+	return (array);
+}
