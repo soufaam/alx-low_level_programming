b714ec48441b8f2bb8a94d7e93d97ccbe32dc7f3
soufiane aamari <soufianeaamari@gmail.com>
2023-04-21 07:58:20 -0700

    function pointer

--- 0x0D-preprocessor/function_pointers.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTION_POINTERS_H_
-#define FUNCTION_POINTER_H_
-#include <stdio.h>
-
-void print_name(char *name, void (*f)(char *));
-#endif /* DOG_H_ */
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
 #ifndef FUNCTION_POINTERS_H_
-#define FUNCTION_POINTER_H_
+#define FUNCTION_POINTERS_H_
 #include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
