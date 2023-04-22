ee47973d5023a3cdcf4249964136c43e0dbc991e
soufiane aamari <soufianeaamari@gmail.com>
2023-04-21 17:02:11 -0700

    pointer function

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "calc.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int argc,char **argv)
+{
+	int a, b;
+	char *s;
+	int (*f)(int , int );
+	if (argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	s = argv[2];
+	if ((s[0] == '/' || s[0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	f = get_op_func(s);
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", (*f)(a,b));
+	return (0);
+}
