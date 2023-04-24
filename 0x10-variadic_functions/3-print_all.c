e175be3ee3831a409e65649a9d7d0b0797e67473
soufiane aamari <soufianeaamari@gmail.com>
2023-04-24 08:04:01 -0700

    fixing some bugs last task of variadec func

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -31,30 +31,34 @@ void print_all(const char * const format, ...)
 	int i = 0, j = 0;
 
 	va_start(ap, format);
-	while (*(format + i) != '\0' && format != NULL)
-	{
-		j = 0;
-		switch (*(format + i))
+	if (format)
+		while (*(format + i) != '\0')
 		{
-			case 'c':
-				printf("%c", va_arg(ap, int));
+			j = 0;
+			switch (*(format + i))
+			{
+				case 'c':
+					printf("%c", va_arg(ap, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(ap, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(ap, double));
+					break;
+				case 's':
+					print_string(va_arg(ap, char *));
+					break;
+				default:
+					j = 1;
+			}
+			while ((*(format + i + 1) != '\0') && (j == 0))
+			{
+				printf(", ");
 				break;
-			case 'i':
-				printf("%d", va_arg(ap, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(ap, double));
-				break;
-			case 's':
-				print_string(va_arg(ap, char *));
-				break;
-			default:
-				j = 1;
+			}
+			i++;
 		}
-		if (*(format + i + 1) != '\0' && j == 0)
-			printf(", ");
-		i++;
-	}
 	va_end(ap);
 	printf("\n");
 }
