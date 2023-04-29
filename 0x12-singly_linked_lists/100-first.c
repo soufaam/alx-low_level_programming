ac66fcdd06edb2e29687dd7f6059905f2cdd05b3
soufiane aamari <soufianeaamari@gmail.com>
2023-04-29 15:28:40 -0700

    first printing

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -12,6 +12,6 @@ void print_hello(void)
 {
 	printf("You're beat! and yet,");
 	printf("you must allow,\n");
-	printf("bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
 
