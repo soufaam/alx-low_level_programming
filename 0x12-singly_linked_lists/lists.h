da151ce61872ec833c73ab37f67566bb8941cbea
soufiane aamari <soufianeaamari@gmail.com>
2023-04-29 15:20:27 -0700

    first before exe

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,5 +24,6 @@ list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void print_hello();
+void print_hello(void) __attribute__ ((constructor));
+
 #endif /* MAIN_H */
