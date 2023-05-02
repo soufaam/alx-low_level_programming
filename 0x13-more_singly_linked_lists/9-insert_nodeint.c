703a1918d4c05d675e7e5c0306404a6331428e5e
soufiane aamari <soufianeaamari@gmail.com>
2023-05-02 15:53:58 -0700

    inserting node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (node)
 		node->n = n;
-	if (!ntmp)
+	if (!ntmp || !head)
 	{
 		*head = node;
 		return (*head);
