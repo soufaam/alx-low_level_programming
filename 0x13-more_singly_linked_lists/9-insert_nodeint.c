beb89f89be7350a7be8fc072dc639c671bfbddaa
soufiane aamari <soufianeaamari@gmail.com>
2023-05-02 05:07:53 -0700

    insert node

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* insert_nodeint_at_index - ENTRYPOINT
+* @head : first param
+* @idx: 2 parama
+* @n: int
+* Return: 0 success or uint
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+	listint_t *node = malloc(sizeof(listint_t)), *ntmp = *head;
+	unsigned int i = 0, insert = 0;
+
+	if (node)
+		node->n = n;
+	while (ntmp)
+	{
+		if (i == idx - 1 && idx != 0)
+		{
+			node->next = ntmp->next;
+			ntmp->next = node;
+			insert = 1;
+		}
+		ntmp = ntmp->next;
+		i++;
+	}
+	if (insert)
+		return (*head);
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,6 +15,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int sum_listint(listint_t *head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
