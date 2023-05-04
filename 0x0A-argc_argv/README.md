4960f7e6c8a5a34ab7ced5c56b62584ee1838ea4
Soufiane aamari <37306375+soufaam@users.noreply.github.com>
2023-05-04 10:23:27 +0000

    Delete 0x0A-argc_argv directory

--- 0x0A-argc_argv/README.md
+++
@@ -1,1 +0,0 @@
-This is a README file for 0x0A-argc argv[] parametr for main function
--- 0x0A-argc_argv/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-#include <stdio.h>
-#include <math.h>
-
-int _putchar(char c);
-
-#endif /* MAIN_H */
