f45b2318b7a635ffbc6ba251ba26700ad5b01f1f
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-06-09 00:10:15 +0100

    fixing betty style

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
-* is_palindrome -  Entrypoint
+* wildcmp -  Entrypoint
 * Description: 'the program's description for int is_palindrome function
 * @s1: 1 param
 *@s2: 2 param
@@ -14,8 +14,8 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 != *s2)
-		return(0);
-	if (*s1 == '*')
-		return (wildcmp(s1 + 1, s2 ) || wildcmp(s1 , s2 + 1));
+		return (0);
+	if (*s1 == '*' || *s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	return (wildcmp(s1 + 1, s2 + 1));
 }
