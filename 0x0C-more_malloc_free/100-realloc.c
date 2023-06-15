5139f30055d0461b7385dacff8b99681e2e2b3e7
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-06-15 12:49:34 +0100

    realloc memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -38,7 +38,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		for (i = 0; i < old_size; i++)
 			newptr[i] =  oldptr[i];
 		if (newptr)
+		{
+			free(ptr);
+			free(oldptr);
 			return (newptr);
+		}
 		free(ptr);
 		return (NULL);
 	}
