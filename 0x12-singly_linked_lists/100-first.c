c254d72324639268a8ae0a18ef1c9132c1864719
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-06-30 17:57:59 +0100

    0x12-singly_linked_lists directory
    This reverts commit c55f11ff96b51e0b38a23257ea4081de8ed5920c.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*print_hello - entrypoint
+*
+*Return: success
+*
+*/
+void print_hello(void)
+{
+	printf("You're beat! and yet, ");
+	printf("you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* _strlen -  Entrypoint
+* Description: 'the program's description _strlen
+* @s : 1 param
+*  Return: Always 0 (Success)
+*/
+
+int _strlen(const char *s)
+{
+	int t = 0, len = 0;
+
+	while (*(s + t) != '\0')
+	{
+		len = len + 1;
+		t++;
+	}
+	return (len);
+}
+/**
+* add_node - ENTRYPOINT
+* @head : constant
+* @str: char
+* Return: 0 success or uint
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *node = malloc(sizeof(list_t));
+
+	if (node)
+	{
+		node->str = strdup(str);
+		node->len = _strlen(str);
+		node->next = *head;
+		*head = node;
+	}
+	if (*head)
+		return (*head);
+	return (NULL);
+}
