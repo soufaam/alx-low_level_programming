3de72ed1cd07772a22a11679018e6371e6a574ca
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-07-04 23:45:18 +0100

    find loop in listint

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+* find_listint_loop - ENTRYPOINT
+* @head : 1 param
+* Return: 0 success or uint
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *store = head, *tmp = NULL;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	while (store != NULL)
+	{
+		tmp = store;
+		store = store->next;
+		if (tmp <= store)
+		{
+			return (store);		
+		}
+	}
+	return (NULL);
+}
