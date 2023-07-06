e9ac611ee57ecb9d92be987219fff90b9ffe3ae8
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-07-07 00:22:34 +0100

    flip bits

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,4 +9,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 void print_binary(unsigned int number);
 int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 #endif /* MAIN_H */
