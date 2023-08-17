da7c6b049cb27f8d62b396e6f1d36f0fd1c81083
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-18 00:47:30 +0100

    insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,5 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tmp = tmp->next;
 		i++;
 	}
+	if (i == idx)
+		return (add_dnodeint_end(h, n));
 	return (NULL);
 }
