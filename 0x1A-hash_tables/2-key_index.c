6c5807ec4c0614cc4044d90ff8f8cbcb2b43e8e2
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-11 00:27:37 +0100

    key index creation

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,51 @@
 #include "hash_tables.h"
 
 /**
+* _strlen -  Entrypoint
+* Description: 'the program's description _strlen
+* @s : 1 param
+*  Return: Always 0 (Success)
+*/
+
+int _strlen(char *s)
+{
+	int t = 0, len = 0;
+
+	while (*(s + t) != '\0')
+	{
+		len = len + 1;
+		t++;
+	}
+	return (len);
+}
+
+/**
+* _strdup -  Entrypoint
+* Description: 'the program's description _strdup
+* @str : 1 param
+*  Return: Always 0 (Success)
+*/
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	s = malloc(sizeof(char) * (_strlen(str)) + 1);
+	if (s)
+	{
+		for (; i < _strlen(str); i++)
+		{
+			*(s + i) = *(str + i);
+		}
+	return (s);
+	}
+	free(s);
+	return (NULL);
+}
+
+/**
 * key_index -  function
 * Description: 'the program's description key_index
 * @key: 1 param
@@ -9,11 +54,7 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	hash_table_t *tab = NULL;
-	hash_node_t *node = NULL;
-	unsigned long int index;
+	unsigned int index = hash_djb2(key);
 
-	index = hash_djb2(key);
-	node = tab->array[index];
-	return (index);
+	return (index % size);
 }
