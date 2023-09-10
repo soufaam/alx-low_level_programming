ca117344f437b6ab7e5d43fc3c5cc32984fd8eb9
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-10 23:53:02 +0100

    create index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,20 +1,19 @@
 #include "hash_tables.h"
 
-
+/**
+* key_index -  function
+* Description: 'the program's description key_index
+* @key: 1 param
+* @size: 2 param
+*  Return: Always 0 (Success)
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	hash_table_t *tab = NULL;
-	int index;
+	hash_node_t *node = NULL;
+	unsigned long int index;
 
 	index = hash_djb2(key);
-	tab = malloc(sizeof(hash_table_t) * size);
-	if (tab)
-	{
-		tab->array = malloc(sizeof(hash_node_t *) * size);
-		if (tab->array)
-		{
-			tab->array->key = key;
-			return (hash_djb2(key));
-		}
-	}
+	node = tab->array[index];
+	return (index);
 }
