9209c085147b3dcd2e0abe4fc76674c72b9d3cd3
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-11 15:14:48 +0100

    set hash table handle collision

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -81,6 +81,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (*key == '\0')
 		return (0);
+	if (!ht)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	ht_node = malloc(sizeof(hash_node_t));
 	if (!ht_node)
