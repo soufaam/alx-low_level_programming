09c0a341c76491cd174143df1706d1909ec222c1
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-12-13 15:47:05 +0100

    100-jump.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,4 +6,5 @@
 #include <math.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 #endif
